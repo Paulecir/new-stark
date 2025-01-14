00510d0c1f45321cac126699a309e03679f50e20
Victor Muniz <victorcmuniz@gmail.com>
2025-01-13 23:13:39 -0300

    fix: update user schema

--- src/presentations/controllers/users/update.schema.ts
+++ src/presentations/controllers/users/update.schema.ts
@@ -5,24 +5,12 @@ export const updateSchema = yup.object().shape({
     name: yup.string().required('Name is required').min(8, 'Name must be at least 8 characters'),
     login: yup.string()
         .required('Login is required')
-        .matches(/^[a-zA-Z0-9]+$/, 'Login must contain only alphanumeric characters')
-        .test('unique', 'Login must be unique', async (value) => {
-            const userExists = await Prisma.user.findUnique({ where: { login: value } });
-            return !userExists;
-        }),
+        .matches(/^[a-zA-Z0-9]+$/, 'Login must contain only alphanumeric characters'),
     email: yup.string()
         .required('Email is required')
-        .email('Invalid email format')
-        .test('unique', 'Email must be unique', async (value) => {
-            const emailExists = await Prisma.user.findUnique({ where: { email: value } });
-            return !emailExists;
-        }),
+        .email('Invalid email format'),
     phone: yup.string()
-        .required('Phone is required')
-        .test('unique', 'Phone must be unique', async (value) => {
-            const phoneExists = await Prisma.user.findUnique({ where: { phone: value } });
-            return !phoneExists;
-        }),
+        .required('Phone is required'),
     country_name: yup.string().required('Country name is required'),
     country_code: yup.string().required('Country code is required'),
 });
