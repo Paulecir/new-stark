8ce8cf4524a86c1e3843fa2e1ac72ebfbc1ecbe7
Victor Muniz <victorcmuniz@gmail.com>
2025-01-16 16:16:41 -0300

    feat: prod

--- src/services/product/createProduct.ts
+++ src/services/product/createProduct.ts
@@ -2,14 +2,7 @@ import Prisma from "@/infra/db/prisma";
 
 export const createProduct = async (data: any) => {
     const model = await Prisma.product.create({
-        data: {
-            name: data.name,
-            description: data.description || null,
-            price: data.price,
-            yield_type: data.yield_type || 'diary',
-            yield: data.yield,
-
-        },
+        data
     });
 
     return model;
