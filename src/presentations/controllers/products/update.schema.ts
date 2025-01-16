bcb944db633d6dc97e04b19745f4e46014668f77
Victor Muniz <victorcmuniz@gmail.com>
2025-01-16 14:19:19 -0300

    feat: correcao

--- src/presentations/controllers/products/update.schema.ts
+++ src/presentations/controllers/products/update.schema.ts
@@ -7,20 +7,11 @@ export const updateSchema = yup.object().shape({
         .max(100, 'O nome deve ter no máximo 100 caracteres.'),
 
     description: yup.string()
-        .nullable()
-        .max(500, 'A descrição deve ter no máximo 500 caracteres.'),
+        .nullable(),
 
     price: yup.number()
         .required('O campo preço é obrigatório.')
         .min(0.01, 'O preço deve ser maior que 0.01.'),
-
-    direct_bonus: yup.boolean()
-        .required('O campo bônus direto é obrigatório.'),
-
-    direct_bonus_yield: yup.number()
-        .required('O rendimento do bônus direto é obrigatório.')
-        .min(0, 'O rendimento do bônus direto deve ser no mínimo 0.')
-        .max(1, 'O rendimento do bônus direto deve ser no máximo 1.'),
 
     yield_type: yup.string()
         .required('O tipo de rendimento é obrigatório.')
@@ -32,15 +23,5 @@ export const updateSchema = yup.object().shape({
     yield: yup.number()
         .required('O rendimento é obrigatório.')
         .min(0, 'O rendimento deve ser no mínimo 0.'),
-
-    unilevel_bonus: yup.boolean()
-        .required('O campo bônus unilevel é obrigatório.'),
-
-    unilevel_bonus_yield: yup.array()
-        .nullable()
-        .of(
-            yup.number()
-                .min(0, 'Cada rendimento do bônus unilevel deve ser no mínimo 0.')
-                .max(1, 'Cada rendimento do bônus unilevel deve ser no máximo 1.')
-        )
+  
 });
