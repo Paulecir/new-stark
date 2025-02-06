f29e692e68d2ac543a1187a09aa7e01da00059d5
root <root@vmi1606547.contaboserver.net>
2025-02-05 23:21:22 -0300

    feat: correcao

--- src/presentations/controllers/balance/add-balance.schema.ts
+++ src/presentations/controllers/balance/add-balance.schema.ts
@@ -1,27 +1,16 @@
 import * as yup from 'yup'
 
 export const schema = yup.object().shape({
-    name: yup.string()
-        .required('O campo nome é obrigatório.')
-        .min(3, 'O nome deve ter no mínimo 3 caracteres.')
-        .max(100, 'O nome deve ter no máximo 100 caracteres.'),
-
-    description: yup.string()
+   
+        description: yup.string()
         .nullable(),
 
-    price: yup.number()
-        .required('O campo preço é obrigatório.')
-        .min(0.01, 'O preço deve ser maior que 0.01.'),
+        user_id: yup.number(),
 
-    yield_type: yup.string()
-        .required('O tipo de rendimento é obrigatório.')
-        .oneOf(
-            ['diary', 'weekly', 'monthly', 'semiannual', 'annual'],
-            'O tipo de rendimento deve ser: diary, weekly, monthly, semiannual ou annual.'
-        ),
+        released_by_id: yup.number(),
 
-    yield: yup.number()
-        .required('O rendimento é obrigatório.')
-        .min(0, 'O rendimento deve ser no mínimo 0.'),
+        amount: yup.number(),
+
+    
 
 });
