95e253e207af217535e462c60ca5a0a00ce7f957
root <root@vmi1606547.contaboserver.net>
2025-02-06 13:16:55 -0300

    feat: correcao

--- src/services/strategies/direct/getDirectById.ts
+++ src/services/strategies/direct/getDirectById.ts
@@ -11,6 +11,21 @@ export const getDirectById = async ({ id, level = 2, user }: any, Prisma = Prism
         where: {
             sponsor_id: first.id
         },
+        include: {
+            Order: {
+                select: {
+                    total: true
+                },
+                where: {
+                    status: "done"
+                }
+            },
+            User: {
+                select: {
+                    id: true
+                }
+            }
+        }
 
     })
 
@@ -23,6 +38,10 @@ export const getDirectById = async ({ id, level = 2, user }: any, Prisma = Prism
         id: node.id,
         level: node.level || parseInt(level.toString()),
         name: node.name, // Nome do usuário
+        email: node.email,
+        phone: node.phone,
+        buyVolume: node.Order.reduce((acc: any, curr) => acc + curr.total.toNumber(), 0),
+        directs: node.User.length,
         avatar: "", // Caso tenha avatar, pode ser adicionado
         points: 0, // Substitua por um cálculo real se necessário
         parentId: node.sponsor_id,
