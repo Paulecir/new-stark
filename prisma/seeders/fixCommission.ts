0fdbba82109a6563cfc4dfcbd5ab3b01201af545
Victor Muniz <victorcmuniz@gmail.com>
2025-03-05 02:54:37 -0300

    feat: correcao

--- prisma/seeders/fixCommission.ts
+++ prisma/seeders/fixCommission.ts
@@ -2,9 +2,10 @@ import { makeCommission } from "../../src/services/commission/makeCommission"
 
 async function arrumar() {
 
-    // for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 20; i++) {
+        console.log("I", i)
         await makeCommission()
-    // }
+    }
     
 }
 
